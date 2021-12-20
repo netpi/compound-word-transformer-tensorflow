a1fce223307952939f8173262dffe16114280497
netpi <moon@netpi.me>
2021-12-20 20:42:03 +0800

    add load-weight

--- dataset.py
+++ dataset.py
@@ -60,19 +60,19 @@ class CP_Word_Dataset():
 
         return r
     
-    def get_tokenizer():
-        pitch_range = range(21, 109)
-        beat_res = {(0, 4): 8, (4, 12): 4}
-        nb_velocities = 32
+def get_tokenizer():
+    pitch_range = range(21, 109)
+    beat_res = {(0, 4): 8, (4, 12): 4}
+    nb_velocities = 32
 
-        additional_tokens = {
-                        'Chord': True,
-                        'Rest': True,
-                        'Tempo': True,
-                        'rest_range': (2, 8),  # (half, 8 beats)
-                        'nb_tempos': 32,  # nb of tempo bins
-                        'tempo_range': (40, 250),  # (min, max)
-                        'Program':False,
-                        }
+    additional_tokens = {
+                    'Chord': True,
+                    'Rest': True,
+                    'Tempo': True,
+                    'rest_range': (2, 8),  # (half, 8 beats)
+                    'nb_tempos': 32,  # nb of tempo bins
+                    'tempo_range': (40, 250),  # (min, max)
+                    'Program':False,
+                    }
 
-        return CPWordEncoding(pitch_range, beat_res, nb_velocities, additional_tokens)
+    return CPWordEncoding(pitch_range, beat_res, nb_velocities, additional_tokens)
